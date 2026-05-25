c1ac333e3fd82bba3d1aed12c9c2304bceb156cc
kennie-afk <kennieme24@gmail.com>
2026-05-25 07:20:31 +0300

    temporary admin bootstrap

--- cmsbackend/src/users/user.routes.ts
+++ cmsbackend/src/users/user.routes.ts
@@ -6,13 +6,14 @@ import {
   updateUser,
   deleteUser,
 } from './user.controller';
+
 import { validate } from '../middleware/validation.middleware';
 import { createUserSchema, updateUserSchema } from './user.schemas';
 import { authenticateToken, authorizeAdmin } from '../middleware/auth.middleware';
 
 const router = Router();
 
-router.post('/', authenticateToken, authorizeAdmin, validate(createUserSchema), createUser);
+router.post('/', validate(createUserSchema), createUser);
 
 router.get('/', authenticateToken, authorizeAdmin, getAllUsers);
 router.get('/:id', authenticateToken, getUserById);
